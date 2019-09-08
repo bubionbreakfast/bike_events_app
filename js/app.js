3b0998dc4900d795629ead625c6d559239ba8f1c
Cameron Pellett <bubionbreakfast@gmail.com>
2019-09-08 08:21:28 +0100

    can make the title text dissapper on mouseover

--- js/app.js
+++ js/app.js
@@ -1,9 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
   const newItemform = document.querySelector('#new-item-form');
+
   newItemform.addEventListener('submit', handleNewItemFormSubmit);
 
   const deleteAllButton = document.querySelector('#delete-all');
   deleteAllButton.addEventListener('click', handleDeleteAllClick);
+
+  const titleTextChange = document.querySelector('h1#title');
+  titleTextChange.addEventListener('onmouseover', handleChangeTitle);
+
+
+
+  const changeTitle = document.querySelector('h1#list-title-text');
+  changeTitle.addEventListener('mouseover', mouseOver);
+  // document.getElementById("h1#list-title-text").addEventListener("mouseover", mouseOver);
+  // document.getElementById("h1#list-title-text").addEventListener("mouseout", mouseOut);
+
+  function mouseOver() {
+    document.getElementById("h1#list-title-text");
+    const changeTitle = document.createElement('list-title-text');
+    changeTitle.textContent = 'Add your Bike To the List';
+    changeTitle.appendChild(titleTextChange)
+  }
+  function mouseOut() {
+    document.getElementById("h1#list-title-text").style.color = "black";
+  }
+
+
+
+
+
+
 })
 
 const handleNewItemFormSubmit = function (event) {
@@ -12,6 +39,10 @@ const handleNewItemFormSubmit = function (event) {
   const bikeListItem = createBikeListItem(event.target);
   const bikeList = document.querySelector('#bike-list');
   bikeList.appendChild(bikeListItem);
+
+  function thanks (handleNewItemFormSubmit){
+    alert('You have entered your bike thank you')
+  };
 
   event.target.reset();
 }
@@ -41,9 +72,22 @@ const createBikeListItem = function (form) {
 }
 
   return bikeListItem;
+
 }
 
 const handleDeleteAllClick = function (event) {
   const readingList = document.querySelector('#bike-list');
   readingList.innerHTML = '';
 }
+
+
+const handleChangeTitle = function (event) {
+  const changeTitle = document.createElement('h1#title');
+  changeTitle.textContent = 'Add your Bike To the List';
+  handleChangeTitle.appendChild(changeTitle);
+  console.log(event);
+};
+
+function mouseOut() {
+  document.getElementById("title").style.color = "black";
+};
